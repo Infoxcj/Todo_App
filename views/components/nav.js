0a3e7d5caac0c9b87675b9b6c354579ef17c9b38
Infoxcj <jonnyx1402@gmail.com>
2023-05-15 19:09:15 -0400

    Integracion de cerrar sesion

--- views/components/nav.js
+++ views/components/nav.js
@@ -71,12 +71,37 @@ const createNavLogin = () => {
 `;
 };
 
+const createNavTodos = () => {
+  navbar.innerHTML = `
+  <div class="max-w-7xl h-16 mx-auto flex items-center px-4 justify-between">
+    <p class="font-bold text-xl text-white">ToDo List</p>
+
+    <!-- version movil -->
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 md:hidden text-white cursor-pointer p-1 hover:bg-green-600 rounded-lg">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+    </svg>
+
+    <!-- version escritorio -->
+    <div class="hidden md:flex flex-row gap-4">
+      <button id="close-btn" class="transition ease-in-out text-white font-bold bg-green-300 hover:bg-green-600 py-2 px-4 rounded-lg">Cerrar Sesión</button>
+    </div>
+
+    <!-- menu movil -->
+    <div class="bg-green-900/60 fixed top-16 right-0 left-0 bottom-0 justify-center items-center gap-4 hidden">
+      <button id="close-btn" class="transition ease-in-out text-white font-bold bg-green-300 hover:bg-green-600 py-2 px-4 rounded-lg">Cerrar Sesión</button>
+    </div>
+  </div>
+`;
+};
+
 if (window.location.pathname === '/') {
   createNavHome();
 } else if (window.location.pathname === '/signup/') {
   createNavSignup()
 } else if (window.location.pathname === '/login/') {
   createNavLogin()
+} else if (window.location.pathname === '/app/') {
+  createNavTodos()
 };
 
 const navBtn = navbar.children[0].children[1];
@@ -93,5 +118,26 @@ navBtn.addEventListener('click', e => {
     navBtn.innerHTML = '<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />'
     menuMobile.classList.remove('flex');
     menuMobile.classList.add('hidden');
+  }
+});
+
+const closeBtnDesktop = navbar.children[0].children[2].children[0];
+const closeBtnMobile = navbar.children[0].children[3].children[0];
+
+closeBtnDesktop.addEventListener('click', async e => {
+  try {
+    await axios.get('/api/logout');
+    window.location.pathname = '/login';
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+closeBtnMobile.addEventListener('click', async e => {
+  try {
+    await axios.get('/api/logout');
+    window.location.pathname = '/login';
+  } catch (error) {
+    console.log(error);
   }
 });
